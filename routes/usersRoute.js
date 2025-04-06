5f5818a9c33fe749bd5fcac1d4a29f1b80bb0c32
root <root@95-216-74-113.ptr>
2025-04-06 08:02:41 +0000

    update profile of existing user

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -5,8 +5,8 @@ const User = require("../models/user");
 
 // Registration
 router.post("/register", async (req, res) => {
-    
-    console.log(`register request received => loginWallet: ${req.body.enterDate}`)
+
+    console.log(`Add user => loginWallet: ${req.body.loginWallet}`)
 
     const newUser = new User({
         userName: req.body.userName,
@@ -29,9 +29,50 @@ router.post("/register", async (req, res) => {
     });
 
     try {
-        const savedUser = await newUser.save()
-        
+        await newUser.save()
+
         res.send("User registered successfully!");
+    } catch (error) {
+        return res.status(400).json({ message: error });
+    }
+});
+
+router.post("/update", async (req, res) => {
+
+    console.log(`Update user => loginWallet: ${req.body.loginWallet}`)
+
+    let user = null;
+    try {
+        user = await User.findOne({ userId: req.body.userId });
+    } catch (error) {
+        console.log(error);
+        return res
+            .status(404)
+            .json({ message: "Could not find the user", success: false });
+    }
+
+    user.userName = req.body.userName;
+    user.userId = req.body.userId;
+    user.enterDate = req.body.enterDate;
+    user.fullName = req.body.fullName;
+    user.emailAddress = req.body.emailAddress;
+    user.permanentAddress = req.body.permanentAddress;
+    user.country = req.body.country;
+    user.mobileNumber = req.body.mobileNumber;
+    user.telegramId = req.body.telegramId;
+    user.twitterId = req.body.twitterId;
+    user.discordId = req.body.discordId;
+    user.loginWallet = req.body.loginWallet;
+    user.btcWallet = req.body.btcWallet;
+    user.solanaWallet = req.body.solanaWallet;
+    user.anotherWallet1 = req.body.anotherWallet1;
+    user.anotherWallet2 = req.body.anotherWallet2;
+    user.referred_by = req.body.referred_by;
+
+    try {
+        await user.save()
+
+        res.send("User update successfully!");
     } catch (error) {
         return res.status(400).json({ message: error });
     }
@@ -42,10 +83,10 @@ router.post("/getuser", async (req, res) => {
     console.log(`get user signal received => ${req.body.address}`)
     try {
         res.send(await User.findOne({ loginWallet: req.body.address }));
-      } catch (error) {
+    } catch (error) {
         return res.status(500).json({ message: error.message });
-      }
-  });
+    }
+});
 
 
 // Login
@@ -90,7 +131,7 @@ router.get("/getAllUsers", async (req, res) => {
 router.get("/getUserCount", async (req, res) => {
     try {
         const currentUsers = await User.find();
-        
+
         res.send(currentUsers.length.toString());
     } catch (error) {
         console.error(error);
