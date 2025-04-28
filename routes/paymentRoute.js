5e326622b8541c7168e4b7a569341be7a2331e7a
root <root@95-216-74-113.ptr>
2025-04-28 08:14:01 +0000

    Distribute button disable after distribute

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const Payment = require("../models/payment");
+const Distribution = require("../models/distribution");
 
 
 // Admin - Delete the current Payment
 router.post("/pay", async (req, res) => {
     console.log(`Deal: ${req.body.dealname} Batch: ${req.body.batch}, Amount: ${req.body.amount}`)
-    
+
     const newPayment = new Payment({
         dealname: req.body.dealname,
         batch: req.body.batch,
@@ -23,5 +24,57 @@ router.post("/pay", async (req, res) => {
         return res.status(400).json({ message: error });
     }
 });
+
+router.get("/canpay/:dealname", async (req, res) => {
+    const { dealname } = req.params;
+    console.log(`Can pay query received => dealname: ${dealname}`)
+
+    const curBatch = await getCurBatch(dealname)
+    
+    console.log(`curBatch: ${curBatch}`)
+    if (curBatch === "") {
+        res.json({
+            can: false
+        })
+    } else {
+        const now = new Date();
+
+        // Find distributions for the dealname, before or equal to now
+        const payments = await Payment.find({
+            dealname: dealname
+        })
+        console.log(`payments: ${payments}`)
+    
+        if (payments.length > 0) {
+            const latest = payments[payments.length - 1];
+            console.log(`latest: ${latest}`)
+            res.json({
+                can: (latest.batch !== curBatch)
+            })
+        }
+        else {
+            res.json({
+                can: true
+            })
+        }
+    }
+});
+
+const getCurBatch = async (dealname) => {
+    const now = new Date();
+
+    // Find distributions for the dealname, before or equal to now
+    const distributions = await Distribution.find({
+        dealname: dealname,
+        date: { $lte: now },
+    }).sort({ date: -1 }); // Sort by date descending
+
+    if (distributions.length > 0) {
+        const latest = distributions[0];
+        return latest.type
+    }
+    
+    return ""
+}
 
 module.exports = router;
